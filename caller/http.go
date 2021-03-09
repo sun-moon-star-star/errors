0e22ad5b571d5bdcb854713555e827fc2a3edd51
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-03-09 20:21:01 +0800

    feat(caller/http): init

--- caller/http.go
+++ caller/http.go
@@ -1,7 +1,36 @@
 package caller
 
-type Http struct{}
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-func (h *Http) Call(*Caller) error {
-	return nil
+type Http struct {
+	NoUseSSL    bool
+	Method      string // default post
+	ContentType string // default application/json
+}
+
+func (h *Http) Call(c *Caller) error {
+	var url string
+	if c.RequestConfig.ServiceName != "" {
+		url = c.RequestConfig.ServiceName
+	} else if c.RequestConfig.IP != "" && c.RequestConfig.Port > 0 {
+		if h.NoUseSSL {
+			url = fmt.Sprintf("http://%s:%d", c.RequestConfig.IP, c.RequestConfig.Port)
+		} else {
+			url = fmt.Sprintf("https://%s:%d", c.RequestConfig.IP, c.RequestConfig.Port)
+		}
+	}
+
+	resp, err := http.Post(url, h.ContentType, bytes.NewReader(c.Req))
+
+	if err != nil {
+		return err
+	}
+
+	c.Resp, err = ioutil.ReadAll(resp.Body)
+	return err
 }
