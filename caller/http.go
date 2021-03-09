1d05e2e99704e032fa700bffd373ca4c57d65699
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-03-10 01:57:36 +0800

    feat(caller/http): Http.url Http.request

--- caller/http.go
+++ caller/http.go
@@ -9,28 +9,56 @@ import (
 
 type Http struct {
 	NoUseSSL    bool
-	Method      string // default post
-	ContentType string // default application/json
+	Method      uint8 // 0 - default post
+	ContentType uint8 // 0 - default application/json
+}
+
+var HTTPUnknownMethodError = fmt.Errorf("%s", "Unknown Method")
+var HTTPUnknownContentTypeError = fmt.Errorf("%s", "Unknown ContentType")
+
+func (h *Http) url(c *Caller) string {
+	var url string
+	if c.RequestConfig.IP != "" && c.RequestConfig.Port > 0 {
+		if h.NoUseSSL {
+			url = fmt.Sprintf("http://%s:%d/%s", c.RequestConfig.IP,
+				c.RequestConfig.Port, c.RequestConfig.ServiceName)
+		} else {
+			url = fmt.Sprintf("https://%s:%d/%s", c.RequestConfig.IP,
+				c.RequestConfig.Port, c.RequestConfig.ServiceName)
+		}
+	} else if c.RequestConfig.ServiceName != "" {
+		url = c.RequestConfig.ServiceName
+	}
+	return url
+}
+
+func (h *Http) request(url string, c *Caller) error {
+	var contentType string
+
+	if h.ContentType == 0 {
+		contentType = "application/json"
+	} else {
+		return HTTPUnknownContentTypeError
+	}
+
+	var err error
+
+	switch h.Method {
+	case 0:
+		resp, err := http.Post(url, contentType, bytes.NewReader(c.Req))
+
+		if err != nil {
+			break
+		}
+
+		c.Resp, err = ioutil.ReadAll(resp.Body)
+	default:
+		return HTTPUnknownMethodError
+	}
+
+	return err
 }
 
 func (h *Http) Call(c *Caller) error {
-	var url string
-	if c.RequestConfig.ServiceName != "" {
-		url = c.RequestConfig.ServiceName
-	} else if c.RequestConfig.IP != "" && c.RequestConfig.Port > 0 {
-		if h.NoUseSSL {
-			url = fmt.Sprintf("http://%s:%d", c.RequestConfig.IP, c.RequestConfig.Port)
-		} else {
-			url = fmt.Sprintf("https://%s:%d", c.RequestConfig.IP, c.RequestConfig.Port)
-		}
-	}
-
-	resp, err := http.Post(url, h.ContentType, bytes.NewReader(c.Req))
-
-	if err != nil {
-		return err
-	}
-
-	c.Resp, err = ioutil.ReadAll(resp.Body)
-	return err
+	return h.request(h.url(c), c)
 }
